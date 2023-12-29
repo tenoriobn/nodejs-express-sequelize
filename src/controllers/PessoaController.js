128d4b44a509f88d1d5a0b00a7c0777d365673cc
Bruno Tenório <tenoriobn@gmail.com>
2023-12-29 09:53:33 -0300

    Desacoplando model e controller

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,10 +1,11 @@
-class PessoaController {
-  static async pegaTodas(req, res) {
-    try {
-      // acessar o modelo
-    } catch (erro) {
-      // erro
-    }
+const Controller = require('./Controller.js');
+const PessoaServices = require('../services/PessoaServices.js');
+
+const pessoaServices = new PessoaServices();
+
+class PessoaController extends Controller {
+  constructor() {
+    super(pessoaServices);
   }
 }
 
