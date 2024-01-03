6ad034b20adbfe4ecc6fe19727bd881e0fa4d822
Bruno Tenório <tenoriobn@gmail.com>
2024-01-03 13:00:56 -0300

    Adicionando mixins

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
       });
 
       Pessoa.hasMany(models.Matricula, {
-        foreignKey: 'estudante_id'
+        foreignKey: 'estudante_id',
+        scope: { status: 'matriculado' },
+        as: 'aulasMatriculadas'
       });
     }
   }
--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -4,6 +4,13 @@ class PessoaServices extends Services {
   constructor() {
     super('Pessoa');
   }
+
+  async pegaMatriculasPorEstudante(id) {
+    const estudante = await super.pegaUmRegistroPorId(id);
+    const listaMatriculas = await estudante.getAulasMatriculadas();
+
+    return listaMatriculas
+  }
 }
 
 module.exports = PessoaServices;
